9855d3cfbd6fccb494f18123884e71d383f87c49
Hasan <pataramrl29>
2023-09-14 10:44:10 +0700

    comomo

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -12,7 +12,7 @@ const Preloader = () => {
 
   return (
     <div className="preloader bg-gray-900 fixed overflow-hidden left-0 right-0 z-50 flex justify-center items-center h-screen w-full">
-    <div className="texts-container loader">
+    <div className="texts-container loader ">
     </div>
   </div>
   )
